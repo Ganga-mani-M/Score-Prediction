92e787d3ac1b01eb2f4f9a0c40557540e28f1959
Ganga Mani <gangamani15@gmail.com>
2026-03-22 14:47:35 +0530

    eda and problem status

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,8 @@
 import sys 
+from src.logger import logging
 import logging
+
+
 def error_message_details(error,error_details:sys):
     _,_,exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -21,6 +24,6 @@ if __name__ =="__main__":
     try:
         a = 1/0
     except Exception as e:  
-        logging.info("Divided by Zero Error") 
+        # logging.info("Divided by Zero Error") 
         raise CustomException(e,sys)
               
